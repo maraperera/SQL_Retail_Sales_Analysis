d9d4cbf203b88f223a97e94c52ad9c9709127587
maraperera <reach.anthony.perera.1@gmail.com>
2025-09-17 11:03:50 +1000

    Data Analysis & Findings

--- SQL Script - Retail Sales Analysis.sql
+++ SQL Script - Retail Sales Analysis.sql
@@ -42,3 +42,45 @@ WHERE
     sale_date IS NULL OR sale_time IS NULL OR customer_id IS NULL OR 
     gender IS NULL OR age IS NULL OR category IS NULL OR 
     quantity IS NULL OR price_per_unit IS NULL OR cogs IS NULL;
+
+
+-- Data Analysis & Findings
+
+-- 01. Sales made on '2022-11-05
+
+SELECT *
+FROM retail_sales
+WHERE sale_date = '2022-11-05';
+
+-- 02. Retrieve all transactions where the category is 'Clothing' and the quantity sold is more than 4 in the month of Nov-2022
+
+SELECT 
+  *
+FROM retail_sales
+WHERE 
+    category = 'Clothing'
+    AND 
+    TO_CHAR(sale_date, 'YYYY-MM') = '2022-11'
+    AND
+    quantity >= 4
+
+-- 03. Calculate the total sales (total_sale) for each category
+
+SELECT 
+    category,
+    SUM(total_sale) as net_sale,
+    COUNT(*) as total_orders
+FROM retail_sales
+GROUP BY 1
+
+-- 04. Find the average age of customers who purchased items from the 'Beauty' category
+
+SELECT
+    ROUND(AVG(age), 2) as avg_age
+FROM retail_sales
+WHERE category = 'Beauty'
+
+-- 05. Find all transactions where the total_sale is greater than 1000
+
+SELECT * FROM retail_sales
+WHERE total_sale > 1000
